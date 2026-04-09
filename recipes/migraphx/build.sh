258eb5fb4d2e1b6c090b81fbf6e48f67c85d50f5
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2026-04-09 11:14:18 +0200

    Build CK JIT library inline in migraphx recipe

--- recipes/migraphx/build.sh
+++ recipes/migraphx/build.sh
@@ -2,12 +2,25 @@
 
 set -xeuo pipefail
 
+# Step 1: Build CK JIT static library.
+# MIGraphX needs the jit_library component which only exists on CK's migraphx
+# branch (https://github.com/ROCm/rocm-libraries#4245).
+# We build and install it into $PREFIX so MIGraphX's
+# find_package(composable_kernel COMPONENTS jit_library) succeeds.
+cmake -GNinja -S ck-jit -B ck-jit/build \
+    ${CMAKE_ARGS} \
+    -DCK_BUILD_JIT_LIB=ON \
+    -DBUILD_TESTING=OFF
+
+cmake --build ck-jit/build -j ${CPU_COUNT}
+cmake --install ck-jit/build
+
+# Step 2: Build MIGraphX
 # cpp-half installs to half_float/half.hpp but MIOpen expects half/half.hpp
 mkdir -p ${PREFIX}/include/half
 ln -sf ${PREFIX}/include/half_float/half.hpp ${PREFIX}/include/half/half.hpp
 
-# Configure with CMake
-cmake -GNinja -S . -B build \
+cmake -GNinja -S migraphx -B migraphx/build \
     ${CMAKE_ARGS} \
     -DGPU_TARGETS=${CONDA_FORGE_DEFAULT_ROCM_GPU_TARGETS} \
     -DBUILD_TESTING=OFF \
@@ -15,11 +28,11 @@ cmake -GNinja -S . -B build \
     -DMIGRAPHX_USE_MIOPEN=ON \
     -DMIGRAPHX_USE_ROCBLAS=ON \
     -DMIGRAPHX_USE_HIPBLASLT=ON \
-    -DMIGRAPHX_USE_COMPOSABLEKERNEL=OFF \
+    -DMIGRAPHX_USE_COMPOSABLEKERNEL=ON \
     -DMIGRAPHX_ENABLE_GPU=ON \
     -DMIGRAPHX_ENABLE_CPU=OFF \
     -DMIGRAPHX_ENABLE_FPGA=OFF
 
-cmake --build build -j ${CPU_COUNT}
+cmake --build migraphx/build -j ${CPU_COUNT}
 
-cmake --install build
+cmake --install migraphx/build
