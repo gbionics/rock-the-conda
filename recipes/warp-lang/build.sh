ea1a7bed16531b75facb6fd82e954351cf5a7ce8
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2026-04-14 18:26:23 +0200

    Pass AMDGPU_TARGETS

--- recipes/warp-lang/build.sh
+++ recipes/warp-lang/build.sh
@@ -7,6 +7,7 @@ if [ "${hip_compiler_version:-None}" != "None" ]; then
     export ROCM_PATH="${BUILD_PREFIX}"
     export HIP_LIB_PATH="${PREFIX}/lib"
     export HIP_INCLUDE_PATH="${PREFIX}/include"
+    export AMDGPU_TARGETS="${CONDA_FORGE_DEFAULT_ROCM_GPU_TARGETS}"
     BUILD_ARGS="${BUILD_ARGS} --hip --ck"
 fi
 
