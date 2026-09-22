86d08f651446634ade7ef291c19723811682ddd4
Silvio Traversaro <silvio.traversaro@gbionics.ai>
2026-09-22 09:57:02 +0200

    Update to latest rattler-build and change architecture propagation logic

--- recipes/hip/activate/hip-rocm-clang_activate.sh
+++ recipes/hip/activate/hip-rocm-clang_activate.sh
@@ -1,16 +1,7 @@
 export HIPCXX_CONDA_BACKUP=${HIPCXX:-}
 export HIPCXX=$CONDA_PREFIX/bin/x86_64-conda-linux-gnu-clang
 
-# Default ROCm GPU targets used by conda-forge packages.
-# The list is given by the official architectures supported by ROCm 7.2.4, see:
-# * https://rocm.docs.amd.com/projects/install-on-linux/en/docs-7.2.4/reference/system-requirements.html
-# * https://rocm.docs.amd.com/projects/radeon-ryzen/en/docs-6.4.4/docs/compatibility/compatibilityryz/native_linux/native_linux_compatibility.html
-# The ryzen link is the one for ROCm 6.4.4, but it is actually the one linked by ROCm 7.2.4 docs.
-# Furthermore, the gfx1102 architecture is added as it was historically present in rock-the-conda
-# rock-the-conda can override this default with its channel-specific target list.
+# ROCm GPU targets are selected by the ROCM_GPU_TARGETS build variant and
+# embedded in this activation script when hip-rocm-clang is packaged.
 export CONDA_FORGE_DEFAULT_ROCM_GPU_TARGETS_CONDA_BACKUP=${CONDA_FORGE_DEFAULT_ROCM_GPU_TARGETS:-}
-if [ -n "${ROCK_THE_CONDA_ROCM_GPU_TARGETS:-}" ]; then
-    export CONDA_FORGE_DEFAULT_ROCM_GPU_TARGETS="${ROCK_THE_CONDA_ROCM_GPU_TARGETS}"
-else
-    export CONDA_FORGE_DEFAULT_ROCM_GPU_TARGETS="gfx908;gfx90a;gfx942;gfx950;gfx1030;gfx1100;gfx1101;gfx1102;gfx1150;gfx1151;gfx1200;gfx1201"
-fi
+export CONDA_FORGE_DEFAULT_ROCM_GPU_TARGETS="@rocm_gpu_targets@"
--- recipes/hip/build.sh
+++ recipes/hip/build.sh
@@ -71,7 +71,9 @@ for CHANGE in "activate" "deactivate"
 do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
     cp "${RECIPE_DIR}/activate/hip_${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/hip_${CHANGE}.sh"
-    cp "${RECIPE_DIR}/activate/hip-rocm-clang_${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/hip-rocm-clang_${CHANGE}.sh"
+    sed -e "s/@rocm_gpu_targets@/${ROCM_GPU_TARGETS}/g" \
+        "${RECIPE_DIR}/activate/hip-rocm-clang_${CHANGE}.sh" \
+        > "${PREFIX}/etc/conda/${CHANGE}.d/hip-rocm-clang_${CHANGE}.sh"
 done
 
 # register the opencl implementation
